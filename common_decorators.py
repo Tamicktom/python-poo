6bc09d77bf67baa155313a17719bd03e795a3cd9
Tamicktom <rickpokemon38@gmail.com>
2024-03-31 23:48:36 -0300

    saving

--- common_decorators.py
+++ common_decorators.py
@@ -2,7 +2,7 @@
 # @staticmethod
 
 class MyClass:
-    value = 10  # class attribute
+    value: int = 10  # class attribute
 
     def __init__(self, name: str):
         self.name = name  # instance attribute
@@ -11,5 +11,18 @@ class MyClass:
     def instance_method(self):
         return f"Instance method called for {self.name}"
 
+    # class method
+    @classmethod
+    def class_method(cls):
+        return f"Class method called for {cls.value}"
+
+    # static method
+    @staticmethod
+    def static_method():
+        return f"Static method called"
+
 
 obj = MyClass('Python')
+print(obj.instance_method())
+print(obj.class_method())
+print(MyClass.class_method())
